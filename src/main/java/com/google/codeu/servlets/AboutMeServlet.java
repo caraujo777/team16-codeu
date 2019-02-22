c853ceb3360c5b52fc211075daacde0dd78ff04e
Oscar Garcia <osjuga24@gmail.com>
2019-02-21 23:55:51 -0500

    Added new servlet for /about

---
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -0,0 +1,22 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * Temp-hard coded msg to test whether things show up under /about
+ */
+
+@WebServlet("/about")
+public class AboutMeServlet extends HttpServlet {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        
+        response.getOutputStream().println("the biggest yeet");
+    }
+}
+
