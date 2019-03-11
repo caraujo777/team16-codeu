69a539e06efe329de8c4942a79222fd7d66ee554
pabloemilio97 <pabloemilioandrade@gmail.com>
2019-03-10 20:29:50 -0600

    Add markdown support for About Me info

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -12,44 +12,47 @@ import com.google.codeu.data.Datastore;
 import com.google.codeu.data.User;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
 
 /**  Handles fetching and saving user data. */
 
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
-  
+
   private Datastore datastore;
-  
+
   @Override
   public void init() {
     datastore = new Datastore();
   }
-  
+
   /** Responds with the "about me" section for a particular user */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
+
     response.setContentType("text/html");
-    
+
     String user = request.getParameter("user");
-    
+
     if(user == null || user.equals("")) {
       // Request is invalid, return empty response
       return;
     }
-    
+
     User userData = datastore.getUser(user);
 
     if(userData == null || userData.getAboutMe() == null) {
       return;
     }
-    
-    response.getOutputStream().println(userData.getAboutMe());   
+
+    response.getOutputStream().println(userData.getAboutMe());
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
+
     UserService userService = UserServiceFactory.getUserService();
     if(!userService.isUserLoggedIn()) {
       response.sendRedirect("/index.html");
@@ -59,10 +62,14 @@ public class AboutMeServlet extends HttpServlet {
     String userEmail = userService.getCurrentUser().getEmail();
     String aboutMe = Jsoup.clean(request.getParameter("about-me"), Whitelist.none());
 
+    Parser parser = Parser.builder().build();
+    Node document = parser.parse(aboutMe);
+    HtmlRenderer renderer = HtmlRenderer.builder().build();
+    aboutMe = renderer.render(document);
+
     User user = new User(userEmail, aboutMe);
     datastore.storeUser(user);
 
     response.sendRedirect("/user-page.html?user=" + userEmail);
   }
 }
-
