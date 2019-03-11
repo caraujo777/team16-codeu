e24cd15cbbda1b50914f618b540cd1dca2e9c53f
Anna Kawakami <annakawakami@Annas-MacBook-Air.local>
2019-03-10 23:48:22 -0400

    Personalized maps page to include pointers that indicate where members of team-16 are based

--- src/main/webapp/js/maps.js
+++ src/main/webapp/js/maps.js
@@ -6,25 +6,36 @@ Last Updated: 03/10/19
 */
 
 let map;
+    /** Creates a map that shows landmarks around Google. */
     function createMap(){
-      map = new google.maps.Map(document.getElementById('map'), {
+    const map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 37.422, lng: -122.084},
         zoom: 15
       });
 
-      const trexMarker = new google.maps.Marker({
-      position: {lat: 37.421903, lng: -122.084674},
-      map: map,
-      animation: google.maps.Animation.DROP,
-      title: 'Stan the T-Rex', 
-      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-      });
+      addLandmark(map, 42.2957057, -71.3216226, "Wellesley College", 
+        "Anna goes to Wellesley College!")
+      addLandmark(map, 41.8239891, -71.4128343, "Brown University",
+      "Cintia goes to Brown University!")
+      addLandmark(map, 40.808805, -73.965899, "Columbia University", 
+      "Oscar goes to Columbia University!")
+      addLandmark(map, 25.1747, -100.5852, "Monterrey Institute of Technology",
+      "Pablo goes to Monterrey Institute of Technology!")
+      addLandmark(map, 37.3688, -122.036, "Sunnyvale, California", 
+      "Gaurav is a Google software engineer based in Sunnyvale California!")
+    }
 
-      var trexInfoWindow = new google.maps.InfoWindow({
-      content: 'This is Stan, the T-Rex statue.'
-      });
-      
-      trexMarker.addListener('click', function() {
-        trexInfoWindow.open(map, trexMarker);
-      });
+    /** Adds a marker that shows an InfoWindow when clicked. */
+    function addLandmark(map, lat, lng, title, description) {
+        const marker = new google.maps.Marker({
+            position: {lat: lat, lng: lng},
+            map: map,
+            title: title
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            content: description
+        });
+        marker.addListener('click', function() {
+        infoWindow.open(map, marker);
+        });
     }
