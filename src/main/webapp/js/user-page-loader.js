aeb1aff704e18b56210f8c9cd57f427cb35f2ab5
pabloemilio97 <pabloemilioandrade@gmail.com>
2019-03-24 15:04:27 -0600

    Make generated message divs styled in user page

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -95,15 +95,19 @@ function fetchAboutMe() {
 function buildMessageDiv(message) {
   const headerDiv = document.createElement('div');
   headerDiv.classList.add('message-header');
+  headerDiv.classList.add('padded');
   var my_message = message.user + ' - ' + new Date(message.timestamp) +
       ' Sentiment Score: [' + message.sentimentScore + ']';
   headerDiv.appendChild(document.createTextNode(my_message));
   const bodyDiv = document.createElement('div');
   bodyDiv.classList.add('message-body');
+  headerDiv.classList.add('padded');
   bodyDiv.innerHTML = message.text;
 
   const messageDiv = document.createElement('div');
   messageDiv.classList.add('message-div');
+  messageDiv.classList.add('rounded');
+  messageDiv.classList.add('panel');
   messageDiv.appendChild(headerDiv);
   messageDiv.appendChild(bodyDiv);
 
