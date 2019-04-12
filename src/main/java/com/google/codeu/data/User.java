0dbf8024245a26e9c16611f1f631511ca4ecc269
Oscar Garcia <osjuga24@gmail.com>
2019-04-11 20:58:27 -0400

    Preemptive User changes for storing Mentions info

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -1,12 +1,27 @@
 package com.google.codeu.data;
 
+import java.util.ArrayList;
+import java.util.UUID;
+
 public class User {
 
   private String email, aboutMe;
+  private ArrayList<UUID> mentions;
 
   public User(String email, String aboutMe) {
     this.email = email;
     this.aboutMe = aboutMe;
+    this.mentions = new ArrayList<UUID>();
+  }
+
+  public User(String email, String aboutMe, List mentions) {
+    this.email = email;
+    this.aboutMe = aboutMe;
+    if (this.mentions != null) 
+      this.mentions.clear();
+    else 
+      this.mentions = new ArrayList()<UUID>;
+    this.mentions.addAll(mentions);
   }
 
   public String getEmail() {
@@ -15,5 +30,13 @@ public class User {
 
   public String getAboutMe() {
     return aboutMe;
+  }
+
+  public List getMentions() {
+    return mentions;
+  }
+
+  public void addMention(UUID messageID) {
+    mentions.add(messageID);
   }
 }
