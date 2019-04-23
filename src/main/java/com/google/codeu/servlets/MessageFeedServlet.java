3de75301650c96943f4f7cfeea53cbc1763bb14c
osjuga <43299093+osjuga@users.noreply.github.com>
2019-04-23 03:35:50 -0400

    Adding mentions to Feed (#84)

--- src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageFeedServlet.java
@@ -15,10 +15,13 @@ import com.google.cloud.language.v1.Sentiment;
 import com.google.cloud.language.v1.Document.Type;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
+import com.google.codeu.data.User;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -92,6 +95,18 @@ public class MessageFeedServlet extends HttpServlet {
     List<BlobKey> blobKeys = blobs.get("image");
 
     Message message = new Message(user, sanitizedText, user, sentimentScore);
+
+    Pattern emailRegex =
+        Pattern.compile(
+            "@(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])");
+    Matcher matcher = emailRegex.matcher(textWithImagesReplaced);
+    while (matcher.find()) {
+      User curUser = datastore.getUser(matcher.group(0).substring(1));
+      if (curUser == null) curUser = new User(matcher.group(0).substring(1), "");
+      curUser.addMention(message.getId().toString());
+      datastore.storeUser(curUser);
+    }
+
     if(blobKeys != null && !blobKeys.isEmpty()) {
       BlobKey blobKey = blobKeys.get(0);
       ImagesService imagesService = ImagesServiceFactory.getImagesService();
